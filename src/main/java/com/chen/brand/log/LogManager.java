ae0465dad6e663a0ff22a0ab5ba4fc691e42354a
陈忠意 <chenzhongyi111@gmail.com>
2017-12-24 23:57:09 +0800

    log

--- src/main/java/com/chen/brand/log/LogManager.java
+++ src/main/java/com/chen/brand/log/LogManager.java
@@ -5,6 +5,8 @@ import com.chen.brand.mapper.LogMapper;
 import com.chen.brand.model.Log;
 import com.chen.brand.model.User;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.apache.log4j.Logger;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -25,6 +27,10 @@ import java.sql.Timestamp;
 @Aspect
 public class LogManager {
 
+    private Logger logger = Logger.getLogger(LogManager.class);
+
+    private Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
     @Autowired
     private LogMapper logMapper;
 
@@ -42,6 +48,7 @@ public class LogManager {
             StringBuffer paramStr = new StringBuffer("");
             Object[] params = jp.getArgs();
             String[] paramNames = ((MethodSignature) jp.getSignature()).getParameterNames();
+            logger.info("======================== params ========================");
             for(int i = 0;i < params.length;++i){
                 Object param = params[i];
                 String paramName = paramNames[i];
@@ -50,17 +57,29 @@ public class LogManager {
                 if(param instanceof Errors) continue;
                 if(param instanceof HttpServletResponse) continue;
                 paramStr.append(paramName + ": " + new Gson().toJson(param) + ", ");
+                logger.info(paramName + ": " + gson.toJson(param));
             }
+            logger.info("======================== params ========================");
             log.setParam(paramStr.toString());
             HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
             User user = (User) request.getSession().getAttribute(Constant.SESSION_NAME);
             log.setUser(user == null ? "-1" : user.getUserName());
+            logger.info("======================== user ========================");
+            logger.info("user: " + gson.toJson(user));
+            logger.info("======================== user ========================");
             log.setMethod(jp.getTarget().getClass().getName() + "." + jp.getSignature().getName() + "()");
+            logger.info("======================== method ========================");
+            logger.info(jp.getTarget().getClass().getName() + "." + jp.getSignature().getName() + "()");
+            logger.info("======================== method ========================");
 
             data = jp.proceed();
 
             Long endTime = System.currentTimeMillis();
             log.setExecutionTime(endTime - startTime);
+
+            logger.info("======================== exeTime ========================");
+            logger.info(endTime - startTime + "ms");
+            logger.info("======================== exeTime ========================");
             //log.setResult(new Gson().toJson(data));
             log.setType(1L);
             logMapper.insert(log);
@@ -68,13 +87,22 @@ public class LogManager {
         }catch (Throwable e){
             Long endTime = System.currentTimeMillis();
             log.setExecutionTime(endTime - startTime);
+            logger.info("======================== exeTime ========================");
+            logger.info(endTime - startTime + "ms");
+            logger.info("======================== exeTime ========================");
             if(e instanceof java.lang.StackOverflowError) {
                 log.setEx(e.toString());
+                logger.info("======================== Exception ========================");
+                logger.info(e.toString());
+                logger.info("======================== Exception ========================");
             }else {
                 Writer writer = new StringWriter();
                 PrintWriter print = new PrintWriter(writer);
                 e.printStackTrace(print);
                 log.setEx(writer.toString());
+                logger.info("======================== Exception ========================");
+                logger.info(writer.toString());
+                logger.info("======================== Exception ========================");
                 writer.close();
                 print.close();
             }
@@ -83,6 +111,7 @@ public class LogManager {
 
             throw e;
         }
+        logger.info("");
         return data;
     }
 }
